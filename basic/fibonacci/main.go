a172be891168f004c3bc81c7462b7956bab16d5f
xavieryin <xavieryin@tencent.com>
2019-09-26 19:22:55 +0800

    update fibonacci

--- basic/fibonacci/main.go
+++ basic/fibonacci/main.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	//方法一
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
 		fmt.Println(f())
 	}
+	fmt.Println("***********")
+	//方法二
+	ch := make(chan int)
+	n := 10
+	go fibonacci2(n, ch)
+	for i := range ch {
+		fmt.Println(i)
+	}
+	time.Sleep(1 * time.Second)
 }
 
 // 闭包，会保留x,y的值
@@ -16,4 +29,14 @@ func fibonacci() func() int {
 		x, y = y, x+y
 		return x
 	}
+}
+
+func fibonacci2(n int, ch chan<- int) {
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	//注意，这里需要手动关闭通道
+	close(ch)
 }
