7b7df22aa284651b61a569e3f8f9be8b70338646
xavieryin <xavieryin@tencent.com>
2020-04-01 20:04:28 +0800

    add 590 & 589

--- algorithms/590. N-ary Tree Postorder Traversal/main.go
+++ algorithms/590. N-ary Tree Postorder Traversal/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 func main() {
 	l1 := &Node{Val: 5}
@@ -16,8 +19,9 @@ func main() {
 		Val:   1,
 		Nodes: []*Node{l, m, r},
 	}
-	postOrder(root)
-	fmt.Println(list)
+	fmt.Println("postOrder : ", postOrder(root))
+	fmt.Println("postorder : ", postorder(root))
+	fmt.Println("preOrder : ", preOrder(root))
 }
 
 type Node struct {
@@ -25,14 +29,91 @@ type Node struct {
 	Nodes []*Node
 }
 
-var list []int
+//https://leetcode.com/problems/n-ary-tree-postorder-traversal/discuss/549341/Go%3A-Iterative-solution-(using-list)
+//https://leetcode.com/problems/n-ary-tree-preorder-traversal/
 
-func postOrder(root *Node) {
+//N叉树后序遍历
+//使用递归的方式来解
+func postOrder(root *Node) []int {
+	var res []int
 	if root == nil {
-		return
+		return res
 	}
-	for _, i := range root.Nodes {
-		postOrder(i)
+	for _, node := range root.Nodes {
+		res = append(res, postOrder(node)...)
 	}
-	list = append(list, root.Val)
+
+	res = append(res, root.Val)
+	return res
+}
+
+func postorder(root *Node) []int {
+	res := []int{}
+	if root == nil {
+		return res
+	}
+
+	stack := list.New()
+	stack.PushFront(root)
+	for stack.Len() > 0 {
+		curr := stack.Remove(stack.Front()).(*Node)
+		if curr != nil {
+			res = append(res, curr.Val)
+		}
+
+		for i := 0; i < len(curr.Nodes); i++ {
+			stack.PushFront(curr.Nodes[i])
+		}
+	}
+
+	reverse(res)
+
+	return res
+}
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+//N叉树前序遍历
+//-------------------------------------------------
+
+func preOrder(root *Node) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+
+	stack := []*Node{root}
+	for len(stack) > 0 {
+		//取出最后一个node
+		r := stack[len(stack)-1]
+		//取出stack中除最后一个剩余的node
+		stack = stack[:len(stack)-1]
+
+		res = append(res, r.Val)
+
+		var tmp []*Node
+		for _, v := range r.Nodes {
+			//将取出的node倒序放到tmp中
+			tmp = append([]*Node{v}, tmp...)
+		}
+		//将tmp放入到stack中
+		stack = append(stack, tmp...)
+	}
+	return res
+}
+
+func preorder(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+	var ret []int
+	ret = append(ret, root.Val)
+	for _, v := range root.Nodes {
+		ret = append(ret, preorder(v)...)
+	}
+	return ret
 }
