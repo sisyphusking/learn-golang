1d450270602b6155f4be0c1d2e26107f1825e754
xavieryin <xavieryin@tencent.com>
2020-04-22 09:23:32 +0800

    add ratelimiter comment

--- examples/ratelimiter/solution3/main.go
+++ examples/ratelimiter/solution3/main.go
@@ -8,7 +8,8 @@ import (
 
 //令牌桶算法
 //用户配置的平均发送速率为r，则每隔1/r秒一个令牌被加入到桶中（每秒会有r个令牌放入桶中），桶中最多可以存放b个令牌。
-//如果令牌到达时令牌桶已经满了，那么这个令牌会被丢弃；
+//用户每次请求都会拿走一个令牌，如果令牌到达时令牌桶已经满了，那么这个令牌会被丢弃；如果令牌不足，那么用户请求就会被拒绝
+//另一个令牌桶算法库：https://github.com/juju/ratelimit
 var limiter = rate.NewLimiter(2, 5)
 
 func limit(next http.Handler) http.Handler {
